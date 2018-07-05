a1e2babf2a89dd1e4e4f3393335c7d5ff96ca6de
Unknown <breno_lopes_10@yahoo.com.br>
2018-07-05 01:01:04 -0300

    Validação meia boca
    Foi feita uma validação do mesmo modo pelo qual é enviado as mensagens do cliente.

--- Projeto_servidor_java/src/cliente/ClienteClass.java
+++ Projeto_servidor_java/src/cliente/ClienteClass.java
@@ -1,18 +1,24 @@
 package cliente;
 
 
+
+
+import java.io.IOException;
 import java.io.PrintStream;
+import java.io.Serializable;
 import java.net.*;
 import java.util.Scanner;
 
 
 
 
-public class ClienteClass {
+public class ClienteClass implements Serializable{
+  
     final int porta;
     final String enderecoIp; 
     Socket cliente;
-    
+    private String login;
+    private String senha;
     /*
      *construtor do socket do cliente 
      *inicializando as variaveis do endereço ip onde o socket irá se conectar e a porta
@@ -31,6 +37,23 @@ public class ClienteClass {
             
         } 
     } 
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
     
         /*
         lê as mensagens que o usuario escrever através do teclado e envia para o servidor
@@ -39,11 +62,11 @@ public class ClienteClass {
       
         try{
             
-        System.out.println("Envie mensagens para o servidor:");          
+           System.out.println("Envie mensagens para o servidor:");          
            Scanner teclado = new Scanner(System.in);
            PrintStream saida = new PrintStream(cliente.getOutputStream());
             while(teclado.hasNextLine()){
-              saida.println(teclado.nextLine());
+            saida.println(teclado.nextLine());
           }                                   
           System.out.println("Enviando mensagem:"  + saida);
       
@@ -53,6 +76,23 @@ public class ClienteClass {
             
         }
     }
+      
+
+    public void Login() throws IOException{
+        //ler login e senha do usuario
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Login: ");        
+        this.setLogin(ler.nextLine());
+        PrintStream saida = new PrintStream(cliente.getOutputStream());
+        saida.println(this.getLogin());
+        
+        System.out.println("Senha:");        
+        this.setSenha(ler.nextLine()); 
+        PrintStream saida2 = new PrintStream(cliente.getOutputStream());
+        saida2.println(this.getSenha());        
+    }
+    
+    
          /*
         Fecha conexão do cliente com o servidor
         */
@@ -65,9 +105,10 @@ public class ClienteClass {
             System.out.println("Erro: " + e.getMessage());
         }
     }
-    public static void main (String arg[])  {
+    public static void main (String arg[]) throws IOException  {
      
        ClienteClass cliente = new ClienteClass();
+       cliente.Login();
        cliente.Mensagem();
        cliente.FechaConexao();
         
--- Projeto_servidor_java/src/servidor/ServidorClass.java
+++ Projeto_servidor_java/src/servidor/ServidorClass.java
@@ -1,17 +1,25 @@
 
     package servidor;
 
-    import java.net.*;
-    import java.util.Scanner;
+
+import java.net.*;
+import java.util.Scanner;
+
 
 
     public class ServidorClass {
         final int porta;
         ServerSocket servidor;
         Socket cliente;
+        String loginServ;
+        String senhaServ;
+
+ 
         
         //construtor inicializando as variaveis e inicia o servidor
         public ServidorClass() {
+        this.loginServ = "breno";
+        this.senhaServ = "123";
         this.porta = 12345;
         try{
         this.servidor = new ServerSocket(porta);
@@ -55,24 +63,43 @@
        
         
         }
+
+        //recebe os dados de login e valida 
+         public void ValidaLogin(){
+                   try{
+                Scanner ler = new Scanner(cliente.getInputStream());
+                String entrada = ler.nextLine();
+                String entrada2 = ler.nextLine();
+                if(entrada.equals(this.loginServ)&&entrada2.equals(this.senhaServ)){
+                System.out.println("Login autenticado com sucesso!");
+                this.RecebeMensagem();
+                }
+                else 
+                    servidor.close();
+          }catch(Exception e){
+              e.printStackTrace();
+              
+          }
+         }
+         
+        
             public void FecharConexao(){
             //fecha conexão do servidor            
             try{
-            System.out.println("Fechando conexão do servidor");
+            System.out.println("Fechando conexão do servidor ...");
             servidor.close();
             System.out.println("Conexão fechada!");
             }catch(Exception e){
                 System.out.println("Erro ao encerrar conexão do servidor!");
                 System.out.println("Erro: " + e.getMessage());
             }
-             
-        
+            
         }  
         public static void main(String arg[]){
             
         ServidorClass servidor = new ServidorClass();
         servidor.ConexaoSocket();
-        servidor.RecebeMensagem();
+        servidor.ValidaLogin();        
         servidor.FecharConexao();
        
     }
